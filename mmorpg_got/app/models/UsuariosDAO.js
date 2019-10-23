b6d11fc8b66691fafdeb17734d445d3e329735bf
AlessandroSampaio <alessandrosampaio521@gmail.com>
2019-10-23 09:37:34 -0300

    Completed : Secao 16

--- mmorpg_got/app/models/UsuariosDAO.js
+++ mmorpg_got/app/models/UsuariosDAO.js
@@ -14,6 +14,29 @@ UsuariosDAO.prototype.inserirUsuario = function(usuario){
     });
 };
 
+UsuariosDAO.prototype.autenticar = function(usuario, req, res){
+    this._connection.connect(function(err, client){
+        if(err){
+            return;
+        }
+        const db = client.db('got');
+        db.collection('usuarios').find(usuario).toArray(function(err, result){
+            if(result[0] != undefined){
+                req.session.autorizado = true;
+                req.session.usuario = result[0].usuario;
+                req.session.casa = result[0].casa;
+            }
+            if(req.session.autorizado){
+                res.redirect('jogo');
+            }else{
+                res.render('Index', {validacao : {}});
+            }
+        });
+        client.close();
+    });
+};
+
+
 module.exports = function(){
     return UsuariosDAO;
 }
